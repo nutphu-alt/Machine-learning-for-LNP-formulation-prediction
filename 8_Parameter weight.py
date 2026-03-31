13d82d81f01fa50ff69f430963e0f25131d1c2fc
Nut Phueakphud <nut_phu@hotmail.com>
2026-04-01 03:33:33 +0700

    Update data file path for loading dataset

--- 8_Parameter weight.py
+++ 8_Parameter weight.py
@@ -10,7 +10,7 @@ from skopt.space import Real
 from skopt import BayesSearchCV
 
 # Load data
-df = pd.read_excel("loaded data")
+df = pd.read_excel("dataset/dataset.xlsx")
 
 # Drop rows with missing outputs
 df = df.dropna(subset=["MFI", "%Positive cells", "%EE", "Size (nm)", "PDI"])
