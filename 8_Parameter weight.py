310e7147349ba642c8ee22a1288d8a8e77c2a0ea
Nut Phueakphud <nut_phu@hotmail.com>
2026-04-01 16:44:04 +0700

    Refactor code for clarity and optimization

--- 8_Parameter weight.py
+++ 8_Parameter weight.py
@@ -5,31 +5,28 @@ from sklearn.model_selection import train_test_split, LeaveOneOut
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import ElasticNet
 from sklearn.compose import TransformedTargetRegressor
+from skopt import BayesSearchCV
 from sklearn.pipeline import Pipeline
 from skopt.space import Real
-from skopt import BayesSearchCV
 
 # Load data
 df = pd.read_excel("dataset/dataset.xlsx")
 
-# Drop rows with missing outputs
 df = df.dropna(subset=["MFI", "%Positive cells", "%EE", "Size (nm)", "PDI"])
 
-# Select features and target
-FEATURES = ["PEG lipid (%)", "Ionizable lipid (%)", "Helper lipid (%)", "pH"]
-TARGET = "MFI"
+features = ["PEG lipid (%)", "Ionizable lipid (%)", "Helper lipid (%)", "pH"]
+target = "MFI"
 
-X = df[FEATURES].values
-y = df[TARGET]
+y = df[target]
 y = y.values.ravel()
 
-# Function for log transform
-def log10_transform(y):
-    return np.log1p(y)
+X = df[features]
 
-# Function to revert log transform
+def log10_transform(y):
+    return np.log10(y + 1e-6)
+
 def inv_log10_transform(y):
-    return np.expm1(y)
+    return np.power(10, y)
 
 en_param = {
     "regressor__Elastic Net__alpha": Real(0.0001, 1000),
@@ -39,47 +36,53 @@ en_param = {
 
 cv = LeaveOneOut()
 
+# Split data into train and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
 
+# Scale data
 model_pipeline = Pipeline([
     ("scaler", StandardScaler()),
     ("Elastic Net", ElasticNet(random_state=42))
 ])
 
+# Transform data into log value
 model = TransformedTargetRegressor(
     regressor=model_pipeline,
     func=log10_transform,
     inverse_func=inv_log10_transform
 )
 
+# Hyperparameter optimization
 en_opt = BayesSearchCV(
     estimator=model,
     search_spaces=en_param,
-    n_iter=50,
+    n_iter=50,  # Bayesian steps
     cv=cv,
     scoring='neg_mean_squared_error',
     n_jobs=-1,
     random_state=42
 )
 
-best_model = en_opt.fit(X_train, y_train)
-y_pred = best_model.predict(X_test)
+# Fit the model
+en_opt.fit(X_train, y_train)
 
-model_coefs = en_opt.best_estimator_.regressor_.named_steps["Elastic Net"].coef_
+# Retrive coefficient value
+en_opt_coefs = en_opt.best_estimator_.regressor_.named_steps["Elastic Net"].coef_
 
 coefs = pd.DataFrame(
-    model_coefs,
+    en_opt_coefs,
     columns=["Coefficients"],
-    index=FEATURES
+    index=features
 )
 
 print(coefs)
 
+# Plot bar graph
 fig, axes = plt.subplots(figsize=(12, 5))
 axes.barh(coefs.index, coefs["Coefficients"], color="crimson")
 axes.set_xlabel("Coefficient values")
 axes.set_ylabel("Features")
-axes.set_xlim(-0.2, 0.2)
+axes.set_xlim(-0.04, 0.04)
 axes.axvline(0, color="dimgray")
 axes.set_title("Feature importance")
 plt.tight_layout()
