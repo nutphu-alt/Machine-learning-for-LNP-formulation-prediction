69007f6317638e9fdb816a9c72d5c442af7d5093
Nut Phueakphud <nut_phu@hotmail.com>
2026-08-16 21:08:28 +0700

    Add prior to Elastic Net hyperparameters

--- 5_Learning curve.py
+++ 5_Learning curve.py
@@ -22,9 +22,9 @@ X = df[["pH", "Helper lipid (%)", "PEG lipid (%)", "Ionizable lipid (%)"]]  # in
 y = df["MFI"]
 
 en_param = {
-    "regressor__Elastic Net__alpha": Real(0.0001, 1000),
+    "regressor__Elastic Net__alpha": Real(0.0001, 1000, prior="log-uniform"),
     "regressor__Elastic Net__l1_ratio": Real(0, 1),
-    "regressor__Elastic Net__tol": Real(0.00001, 0.001)
+    "regressor__Elastic Net__tol": Real(0.00001, 0.001, prior="log-uniform")
 }
 
 def log10_transform(y):
